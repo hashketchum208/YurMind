d6b4fd31ad07b22eda78270eaa9e941b05917d60
Leo <111384016+hashketchum208@users.noreply.github.com>
2023-04-10 23:49:43 -0600

    updated some functionality

--- routes/api/notes.js
+++ routes/api/notes.js
@@ -1,20 +1,52 @@
 const path = require('path');
-const router = require('express').Router();
+const fb = require('express').Router();
 const fs = require('fs');
 
- 
-    router.post("/notes", (req, res) => {
-      const db = fs.readFileSync(path.join(process.cwd(), "./db/db.json"));
-  
-      const newNotes = [
-          ...db,
-          { 
-            title: req.body.title, 
-            text: req.body.text 
-          },
-      ];
-      fs.writeFile(path.join(process.cwd(), "./db/db.json"), newNotes);
-    })
+fb.get('/', (req, res) => {
+  // console.info(`GET /api/note`);
+  // res.status(200).json(note);
+  readFromFile('./db/db.json').then((data) => res.json(JSON.parse(data)))
+}); 
 
-module.exports = router;
-    
+  fb.post('/', (req,res) => {
+
+    const db = fs.readFileSync('db/db.json');
+    db = JSON.parse(db);
+    res.json(db)
+    const newNote = {
+      title: req.body.title,
+      text: req.body.text,
+      //will adding UUID fix this problem?/ what are some of the packages??
+      unique: true,
+    };
+
+    fs.writeFileSync(path.join('db/db.json', JSON.stringify(db)));
+    db.push(newNote)
+});
+
+module.exports = fb;
+
+
+// const path = require('path');
+// const router = require('express').Router();
+// const fs = require('fs');
+
+ 
+//     router.post("/notes", (req, res) => {
+//       const db = fs.readFileSync(path.join(process.cwd(), "./db/db.json"));
+  
+//       const newNotes = [
+//           ...db,
+//           { 
+//             title: req.body.title, 
+//             text: req.body.text 
+//           },
+//       ];
+
+//       fs.writeFile(path.join("./db/db.json", newNotes, (err, data) => {
+//         if (err) throw err;
+//         console.log(data);
+//     }));
+//   })
+
+// module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,5 @@ const notes = require('./api/notes')
 const app = express();
 
 app.use('./api/notes.js', notes);
-// const api = require('./api')
 
 module.exports = app;
