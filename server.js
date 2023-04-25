65b3975be1d2001e583d672699609d9167f4776c
Leo <111384016+hashketchum208@users.noreply.github.com>
2023-04-25 13:02:06 -0600

    changed note to notes in server

--- server.js
+++ server.js
@@ -21,7 +21,7 @@ app.get("/", (req, res) =>
 
 // GET Route for note page
 app.get("/notes", (req, res) =>
-  res.sendFile(path.join(__dirname, "./public/note.html"))
+  res.sendFile(path.join(__dirname, "./public/notes.html"))
 );
 
 app.get("/api/notes", (req, res) =>
