c943fe5cc46bc9a1c8b66b2c3e4e60a7c74a4349
Leo <111384016+hashketchum208@users.noreply.github.com>
2023-04-25 13:46:58 -0600

    server erron on railway, trying to find bug

--- routes/index.js
+++ routes/index.js
@@ -2,5 +2,6 @@ const express = require('express')
 const notes = require('./api/notes')
 const app = express();
 app.use('./api/notes.js', notes);
+const api = require('./api')
 
 module.exports = app;
